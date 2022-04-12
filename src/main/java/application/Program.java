127a6828801571695bca4be29023eb03a1b69e3b
Daniel Diniz <daniel.diniz1@icloud.com>
2022-04-12 15:13:12 -0300

    utilizando try-with-resources

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,28 +1,23 @@
 package application;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        File file = new File("C:\\Users\\danie\\Documents\\Daniel_Diniz\\java\\Prof-Nelio-alves\\leitura-documentos\\in.txt");
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(file);
-            while (scanner.hasNextLine()){
-                String line = scanner.nextLine();
+        String path = "C:\\Users\\danie\\Documents\\Daniel_Diniz\\java\\Prof-Nelio-alves\\leitura-documentos\\in.txt";
+
+
+        try (BufferedReader bf = new BufferedReader(new FileReader(path))) {
+            String line = bf.readLine();
+            while (line != null){
                 System.out.println(line);
+                line = bf.readLine();
             }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            if (scanner != null){
-                scanner.close();
-            }
+        }catch (IOException ex){
+            System.out.println("ERRO: " + ex.getMessage());
         }
     }
 }
